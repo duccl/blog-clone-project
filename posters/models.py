e06a67854cbe7dd7f602ea374eba20e3477d894d
ecoltri2 <eduardo.coltri@dxc.com>
2020-05-26 08:56:01 -0300

    Showing only published posts

--- posters/models.py
+++ posters/models.py
@@ -6,7 +6,8 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=60,unique=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    published_date = models.DateField(auto_now=True)
+    published_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now=True)
     body = models.TextField(max_length=3000)
     is_publicated = models.BooleanField(default=False)
 
