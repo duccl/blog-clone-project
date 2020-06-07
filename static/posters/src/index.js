80e4c9e22895ba378a46fab528849f1bf8bf8f1d
ecoltri2 <eduardo.coltri@dxc.com>
2020-06-07 16:19:05 -0300

    Using ckeditor4 as rich text editor

--- static/posters/src/index.js
+++ static/posters/src/index.js
@@ -2,4 +2,15 @@ const get_content = () => {
     $("#content").html($("#content").text())
 }
 
+const textareaPresent = () =>{
+    return $('textarea').length == 1;
+}
+
+window.onload = () => {
+    if(textareaPresent()){
+        let textAreaName = $('textarea').attr('name')
+        CKEDITOR.replace(textAreaName);
+    }
+}
+
 get_content()
