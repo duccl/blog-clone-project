6fea58b1054aa09874d88a432cf8ca2df2aeb93d
ecoltri2 <eduardo.coltri@dxc.com>
2020-06-09 20:13:41 -0300

    Icon Image in browser tab

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'ob4%g0wbxazptjp311a%z(6i0bt)c&vi=e7%kjmi^l5*9bgob-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['duccl.pythonanywhere.com']
+ALLOWED_HOSTS = ['duccl.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
